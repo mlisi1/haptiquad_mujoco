eb1ffe0672269300096c1c1dbc373319bd460238
m.lisi1 <m.lisi1@studenti.unipi.it>
2025-02-23 18:16:28 +0100

    added submodules; added building bash script

---
+++ build.sh
@@ -0,0 +1,5 @@
+colcon build --packages-skip momobs_ros2 mujoco description --event-handlers desktop_notification-
+source install/setup.bash
+colcon build --symlink-install --packages-select description mujoco --event-handlers desktop_notification-
+source install/setup.bash
+colcon build --cmake-force-configure --packages-select momobs_ros2
