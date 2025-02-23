558c9b0162f8b1552d05fb8b7fd7da5695f6f1b0
m.lisi1 <m.lisi1@studenti.unipi.it>
2025-02-23 20:17:56 +0100

    updated and added submodules; added anymal_c_config package

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+colcon build --packages-select champ --event-handlers desktop_notification-
+source install/setup.bash
 colcon build --packages-skip momobs_ros2 mujoco description --event-handlers desktop_notification-
 source install/setup.bash
 colcon build --symlink-install --packages-select description mujoco --event-handlers desktop_notification-
